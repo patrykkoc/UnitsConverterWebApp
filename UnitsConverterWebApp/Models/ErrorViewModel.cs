247af20cf41aa22346bf93c9ccfbc869cd2d2f73
Patryk K <patryk.koc44@gmail.com>
2025-06-06 12:47:24 +0200

    Add project files.

---
+++ UnitsConverterWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnitsConverterWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
