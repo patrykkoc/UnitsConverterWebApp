7865db0119b58bd3b925d8b2d72d95fc80161fc3
aruhs10000 <aruhs10000@gmail.com>
2025-06-09 13:41:25 +0200

    Seed data corected unit tobasea from base

--- UnitsConverterWebApp/Models/SeedData.cs
+++ UnitsConverterWebApp/Models/SeedData.cs
@@ -36,47 +36,47 @@ namespace UnitsConverterWebApp.Models
                 if (!context.Units.Any())
                 {
                     context.Units.AddRange(
-                  new Unit
-                  {
+                        // DISTANCE
+                        new Unit
+                        {
+                            Name = "Meter",
+                            Symbol = "m",
+                            MultiplierToBase = 1,
+                            OffsetToBase = 0,
+                            IsBaseUnit = true,
+                            CategoryId = distanceCategory.Id
+                        },
+                        new Unit
+                        {
+                            Name = "Centimeter",
+                            Symbol = "cm",
+                            MultiplierToBase = 0.01,
+                            OffsetToBase = 0,
+                            IsBaseUnit = false,
+                            CategoryId = distanceCategory.Id
+                        },
 
-                      Name = "Meter",
-                      Symbol = "m",
-                      MultiplierToBase = 1,
-                      OffsetToBase = 0,
-                      IsBaseUnit = true,
-                      CategoryId = distanceCategory.Id
-                  },
-                  new Unit
-                  {
+                        // TEMPERATURE
+                        new Unit
+                        {
+                            Name = "Celsius",
+                            Symbol = "°C",
+                            MultiplierToBase = 1,
+                            OffsetToBase = 0,
+                            IsBaseUnit = true,
+                            CategoryId = temperatureCategory.Id
+                        },
+                        new Unit
+                        {
+                            Name = "Fahrenheit",
+                            Symbol = "°F",
+                            MultiplierToBase = 5.0 / 9.0,           
+                            OffsetToBase = -32 * (5.0 / 9.0),       
+                            IsBaseUnit = false,
+                            CategoryId = temperatureCategory.Id
+                        }
+                    );
 
-                      Name = "Centimeter",
-                      Symbol = "cm",
-                      MultiplierToBase = 0.01,
-                      OffsetToBase = 0,
-                      IsBaseUnit = false,
-                      CategoryId = distanceCategory.Id
-                  },
-                  new Unit
-                  {
-
-                      Name = "Celsius",
-                      Symbol = "°C",
-                      MultiplierToBase = 1,
-                      OffsetToBase = 0,
-                      IsBaseUnit = true,
-                      CategoryId = temperatureCategory.Id
-                  },
-                  new Unit
-                  {
-
-                      Name = "Fahrenheit",
-                      Symbol = "°F",
-                      MultiplierToBase = 5.0 / 9.0,
-                      OffsetToBase = -32 * (5.0 / 9.0),
-                      IsBaseUnit = false,
-                      CategoryId = temperatureCategory.Id
-                  }
-              );
                     context.SaveChanges();
                 }
 
